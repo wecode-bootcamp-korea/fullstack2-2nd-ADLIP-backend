52193597957ba48cdb6d07a0ba4f6bb3068dc4ff
hwibaski <hwibaski@gmail.com>
2021-11-04 15:33:33 +0900

    title 컬럼 추가

--- prisma/migrations/20211102184959_make_relation_between_comment_image_and_comment/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `product_options` MODIFY `start_at` TIME NOT NULL;
-
--- AddForeignKey
-ALTER TABLE `comment_images` ADD CONSTRAINT `comment_images_comment_id_fkey` FOREIGN KEY (`comment_id`) REFERENCES `comments`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
