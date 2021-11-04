f233f9d09453aa0b36ebd83221be650b56880cc2
hwibaski <hwibaski@gmail.com>
2021-11-04 14:42:51 +0900

    컨플릭트 해결중

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,12 @@ const getProductById = async (req, res, next) => {
     const { id } = req.params;
     const productById = await productService.getProductById(id);
     res.status(200).json(productById);
-    
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
 const getCommentsById = async (req, res, next) => {
   try {
     const { id } = req.params;
