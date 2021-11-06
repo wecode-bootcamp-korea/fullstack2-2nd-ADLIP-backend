ec9ee4afc4ab5f20c5b0cb48f523da346c195597
hwibaski <hwibaski@gmail.com>
2021-11-06 23:46:16 +0900

    Modify: 코멘트 데이터 날짜 형식 수정

--- services/userService.js
+++ services/userService.js
@@ -59,6 +59,7 @@ const signInKakao = async (userInfo, body) => {
   if (isExistUser.socialPlatform !== 'kakao') {
     throw new Error('이미 존재하는 아이디입니다.');
   }
+  return createJwtToken(email, status);
 };
 
 export default { createUser, signInUser, signInKakao };
